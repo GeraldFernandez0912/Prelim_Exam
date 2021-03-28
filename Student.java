3f95b5f7f18e59174a3bbc0a459fdf6223c405cd
Gerald B. Fernandez <fernandezgerald02@gmail.com>
2021-03-28 21:15:53 +0800

    1st commit

---
+++ Student.java
@@ -0,0 +1,60 @@
+class Student
+{
+public int IDnumber;
+public int hours;
+public int points;
+public static int lastStudentID;
+ 
+public void setIDnumber()
+{
+lastStudentID += 1;
+IDnumber = lastStudentID;
+}
+ 
+public int getIDnumber()
+{
+return IDnumber;
+}
+ 
+public void setHours(int number)
+{
+hours = number;
+}
+ 
+public int getHours()
+{
+return hours;
+}
+ 
+public void setPoints(int number)
+{
+points = number;
+}
+ 
+public int getPoints()
+{
+return points;
+}
+ 
+ 
+public void showIDnumber()
+{
+System.out.println("ID Number: " + IDnumber);
+}
+ 
+public void showHours()
+{
+System.out.println("Credit Hours: " + hours);
+}
+ 
+public void showPoints()
+{
+System.out.println("Points Earned: " + points);
+}
+ 
+public double getGradePoint()
+{
+return (double) (points / hours);
+}
+ 
+}
